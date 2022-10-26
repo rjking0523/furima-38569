24654ae078cdda0656be82d0fa8a99da087d97d6
rjking0523 <kt3422.wase@gmail.com>
2022-10-26 15:31:25 +0900

    modelの修正（withoptionsの集約）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: { message: 'を入力してください' }
-
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります', allow_blank: true }
 
   with_options presence: { message: 'を入力してください' } do
     validates :first_name
     validates :last_name
+    validates :nickname
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birth_date
   end
 
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'に全角文字を使用してください', allow_blank: true } do
@@ -17,16 +19,9 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-
-  with_options presence: { message: 'を入力してください' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-
   with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください', allow_blank: true } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
-  validates :birth_date, presence: { message: 'を入力してください' }
 end
