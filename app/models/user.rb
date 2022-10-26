69cc40354ff51c426382ccb51e5147f8bd8c61d6
rjking0523 <kt3422.wase@gmail.com>
2022-10-26 12:07:26 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: {message: 'を入力してください'}
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
+  with_options presence: {message: 'を入力してください'}, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'に全角文字を使用してください'} do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: {message: 'を入力してください'}, format: {with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください'} do
+  validates :first_name_kana
+  validates :last_name_kana
+  end
+
+  validates :birth_date, presence: {message: 'を入力してください'}
 end
