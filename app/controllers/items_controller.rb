ea0bde5ff7f6c8ea151982f88af37618ea9ad326
rjking0523 <kt3422.wase@gmail.com>
2022-10-27 13:43:33 +0900

    ログアウト時に出品ボタン押下した際の挙動変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_registration
-    redirect_to new_user_registration_path unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
